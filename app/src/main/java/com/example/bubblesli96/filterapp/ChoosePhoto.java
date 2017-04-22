226601099c4ae3466d4f0a2aee5d4508557e2f29
Julia Molian Li <bubblesli96@Julias-MacBook-Air.local>
2017-04-21 22:12:40 -0700

    created new activity MyCameraActivity.java and made changes to the manifest file

---
+++ app/src/main/java/com/example/bubblesli96/filterapp/ChoosePhoto.java
@@ -0,0 +1,17 @@
+package com.example.bubblesli96.filterapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChoosePhoto extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_photo);
+    }
+
+
+}
+
+
---
+++ app/src/main/java/com/example/bubblesli96/filterapp/MyCameraActivity.java
@@ -0,0 +1,60 @@
+package com.example.bubblesli96.filterapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.net.Uri;
+
+public class MyCameraActivity extends Activity {
+    private static final int TAKE_PHOTO = 1010;
+    private static final int CHOOSE_PHOTO = 1011;
+    private ImageView imageView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        this.imageView = (ImageView)this.findViewById(R.id.imageView1);
+        Button takePicture = (Button) this.findViewById(R.id.button1);
+        takePicture.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent takePicture = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(takePicture, TAKE_PHOTO);//zero can be replaced with any action code
+            }
+        });
+        Button choosePhoto = (Button) this.findViewById(R.id.button2);
+        choosePhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent pickPhoto = new Intent(Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(pickPhoto , CHOOSE_PHOTO);
+            }
+        });
+
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) {
+        super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
+        switch(requestCode) {
+            case TAKE_PHOTO:
+                if(resultCode == RESULT_OK){
+                    Uri selectedImage = imageReturnedIntent.getData();
+                    imageView.setImageURI(selectedImage);
+                }
+
+                break;
+            case CHOOSE_PHOTO:
+                if(resultCode == RESULT_OK){
+                    Uri selectedImage = imageReturnedIntent.getData();
+                    imageView.setImageURI(selectedImage);
+                }
+                break;
+        }
+    }
+}
